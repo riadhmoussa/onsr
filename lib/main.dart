c72caf134a23ef8882083d7e1b90d95bec4064a0
Moussa Riadh <riadhmoussa1177@gmail.com>
2024-06-25 16:59:28 +0100

    add new feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,53 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:onsr/routes/app_pages.dart';
 import 'package:onsr/routes/app_routes.dart';
 import 'package:onsr/utils/my_http_overrides.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message: ${message.messageId}');
+}
+
+
+Future<void> _requestPermissions() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  print('User granted permission: ${settings.authorizationStatus}');
+}
+void main() async  {
   HttpOverrides.global = MyHttpOverrides();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  // Subscribe to the topic 'millingData'
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  await messaging.subscribeToTopic('allUsers');
+
+  await _requestPermissions();
+
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -25,4 +60,6 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+
 }
+
